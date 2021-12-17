39ade246f94105d0c65312317a001411331b1561
MarcosCela <marcos.cela@datadoghq.com>
2021-12-17 13:29:02 +0100

    Include a test with a wf with search attributes

--- temporaltest/server_test.go
+++ temporaltest/server_test.go
@@ -10,6 +10,7 @@ import (
 	"testing"
 	"time"
 
+	"go.temporal.io/api/enums/v1"
 	"go.temporal.io/sdk/client"
 	"go.temporal.io/sdk/worker"
 
@@ -110,5 +111,58 @@ func BenchmarkRunWorkflow(b *testing.B) {
 				b.Fatal(err)
 			}
 		}(b)
+	}
+}
+
+func TestWithSearchAttributes(t *testing.T) {
+	// Create test Temporal server and client
+	ts := temporaltest.NewServer(temporaltest.WithT(t),
+		temporaltest.WithSA(map[string]enums.IndexedValueType{
+			"test": enums.INDEXED_VALUE_TYPE_TEXT,
+		}))
+	c := ts.Client()
+
+	// Register a new worker on the `hello_world` task queue
+	ts.Worker("hello_world", func(registry worker.Registry) {
+		helloworld.RegisterWorkflowsAndActivities(registry)
+	})
+
+	// Start test workflow with search attributes
+	wfr, err := c.ExecuteWorkflow(
+		context.Background(),
+		client.StartWorkflowOptions{
+			TaskQueue:        "hello_world",
+			SearchAttributes: map[string]interface{}{"test": "test-value"},
+		},
+		helloworld.Greet,
+		"world",
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Get workflow result
+	var result string
+	if err := wfr.Get(context.Background(), &result); err != nil {
+		t.Fatal(err)
+	}
+
+	// Print result
+	fmt.Println(result)
+	// Output: Hello world
+	searchAttrs, err := ts.Client().GetSearchAttributes(context.Background())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Ensure that the search attributes returned by the server
+	// have the search attribute defined in the test
+	value, ok := searchAttrs.Keys["test"]
+	if ok {
+		if value != enums.INDEXED_VALUE_TYPE_TEXT {
+			t.Fatal(fmt.Sprintf("search attribute was defined and present, but the value did not match. Expected %s, but got %s", enums.INDEXED_VALUE_TYPE_TEXT, value.String()))
+		}
+	} else {
+		t.Fatal("search attribute was defined, but not returned by the server")
 	}
 }
