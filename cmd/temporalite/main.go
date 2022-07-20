14f3a37a74359b007ad576762ac8f8403cd9111f
Rob Holland <rob@temporal.io>
2022-07-20 15:22:39 +0100

    Fix incorrect module name change.

--- cmd/temporalite/main.go
+++ cmd/temporalite/main.go
@@ -28,7 +28,7 @@ import (
 
 // Name of the ui-server module, used in tests to verify that it is included/excluded
 // as a dependency when building with the `headless` tag enabled.
-const uiServerModule = "github.com/temporalio/v2/ui-server"
+const uiServerModule = "github.com/temporalio/ui-server"
 
 var (
 	defaultCfg *liteconfig.Config
