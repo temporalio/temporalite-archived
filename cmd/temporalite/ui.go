08e491b5bc385bb9aa39d18b86a630d11f29bcef
Rob Holland <rob@temporal.io>
2022-07-27 15:17:34 +0100

    UI bump (#93)

--- cmd/temporalite/ui.go
+++ cmd/temporalite/ui.go
@@ -5,9 +5,9 @@ package main
 import (
 	// This file should be the only one to import ui-server packages.
 	// This is to avoid embedding the UI's static assets in the binary when the `headless` build tag is enabled.
-	uiserver "github.com/temporalio/ui-server/server"
-	uiconfig "github.com/temporalio/ui-server/server/config"
-	uiserveroptions "github.com/temporalio/ui-server/server/server_options"
+	uiserver "github.com/temporalio/ui-server/v2/server"
+	uiconfig "github.com/temporalio/ui-server/v2/server/config"
+	uiserveroptions "github.com/temporalio/ui-server/v2/server/server_options"
 
 	"github.com/DataDog/temporalite"
 )
